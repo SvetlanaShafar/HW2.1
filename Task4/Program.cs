1fb93282f32f62c7d8643d5ac204a2f7347c6454
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-02 17:45:19 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if (N % 2 == 0)
+{
+Console.WriteLine("Число четное");
+}
+else 
+{
+Console.Write("Число нечетное");
+}
