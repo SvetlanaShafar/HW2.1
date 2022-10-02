9c337ec72b55758c4656d50d6409487217c586d9
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-02 17:23:33 +0300

    исправление опечатки

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите три числа числи:");
+﻿Console.WriteLine("Введите три числа:");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
 int c = Convert.ToInt32(Console.ReadLine());
