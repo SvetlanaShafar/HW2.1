4253bce3aa042eb9d0c46c46cad9ad63ca7ba34d
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-02 17:07:38 +0300

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое числи:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе числи:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+Console.Write("max= ");
+Console.WriteLine(a);
+}
+else 
+{
+Console.Write("max= ");
+Console.WriteLine(b);
+}
