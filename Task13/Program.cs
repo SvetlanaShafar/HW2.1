0bb96d1175906e56b5010bbee85a863f870290bd
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-04 18:17:20 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+string m = Convert.ToString(a);
+if (m.Length > 2)
+{
+Console.WriteLine($"Третья цифра: {m[2]}");
+}
+
+else
+{
+Console.WriteLine("Третьей цифры нет");
+}
