90d6199b5be865b3c9a388afbe71f19e55f151b3
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-04 18:32:04 +0300

    add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите любую цифру:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a==6 || a==7)
+
+  Console.WriteLine("Сегодня выходной");  
+
+if (a < 1 || a > 7)
+
+    Console.WriteLine("Такого дня не существует");
+
+if (a > 1 && a < 5 )
+
+   Console.WriteLine("Сегодня будний день");
