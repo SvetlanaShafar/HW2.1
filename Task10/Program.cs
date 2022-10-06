0d061bffe8d6932f0ebe3f8d52b59b10dbda06d2
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-06 08:53:16 +0300

    task 10 дополнила

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите трехзначное число:");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Вторая цифра: {a=(a/10) % 10}");
+if (a > 99)
+    Console.WriteLine($"Вторая цифра: {a = (a / 10) % 10}");
+else 
+Console.WriteLine("Введите трехзначное число!");
