0b2878e684f63243ecd9145c3e93efc4d1121d19
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-06 09:00:45 +0300

    +1 изменение к Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите трехзначное число:");
 int a = Convert.ToInt32(Console.ReadLine());
-if (a > 99)
+if (a > 99 && a < 1000)
     Console.WriteLine($"Вторая цифра: {a = (a / 10) % 10}");
 else 
 Console.WriteLine("Введите трехзначное число!");
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,11 +3,10 @@ Console.WriteLine("Введите любое число:");
 int a = Convert.ToInt32(Console.ReadLine());
 string m = Convert.ToString(a);
 if (m.Length > 2)
-{
+
 Console.WriteLine($"Третья цифра: {m[2]}");
-}
+
 
 else
-{
+
 Console.WriteLine("Третьей цифры нет");
-}
