d65d9f2919aaddad731ff49492f6e8adc6ba0951
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-08 19:33:59 +0300

    add Task19,Task21,Task23

---
+++ Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+int ost = 0;
+int c = a;
+
+
+if (a % 10 == 0)
+    Console.WriteLine("Число не палиндром");
+else
+{
+    while (a > 0)
+    {
+        ost = a % 10;
+        b = b * 10 + ost;
+        a = a / 10;
+    }
+
+    if (c == b)
+        Console.WriteLine("Число палиндром");
+    else
+        Console.WriteLine("Число не палиндром");
+
+}
+
---
+++ Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите x =");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y =");
+double y = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z =");
+double z = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x1 =");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите y1 =");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z =");
+double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"  Расстояние в 3D пространстве: {Math.Sqrt((x - x1) * (x - x1) + (y - y1) * (y - y1) + (z - z1) * (z - z1))} ");
