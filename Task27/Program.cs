4d810032c7a076b4d96b392ddc76454682e1f0e3
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-12 10:27:44 +0300

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите  число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+if (a < 0)
+    a = a * (-1);
+while (a > 0)
+{
+
+    int c = a % 10;
+    b = b + c;
+    a = a / 10;
+}
+Console.WriteLine(b);
