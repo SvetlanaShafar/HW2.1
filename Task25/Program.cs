b084409b43132d3f0e2654ba8151852660f74566
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-12 10:02:31 +0300

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = 1;
+
+for (int i = 1; i <= b; i++)
+
+    result = result * a;
+
+
+Console.WriteLine();
+
+Console.WriteLine("result = " + result);
+
