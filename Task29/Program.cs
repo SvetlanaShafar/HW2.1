95d93f79455a30c1092c48bbcd317a21c05a25f7
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-12 11:15:26 +0300

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите цифру 8, для соответсвия условию задачи:");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+int i = 0;
+while (a < 8 || a > 8)
+{
+    Console.WriteLine("Введите цифру 8, для соответсвия условию задачи:");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+while (i < array.Length)
+{
+    Console.WriteLine("Введите " + i + " элемент массива :");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+
+Console.Write(" Массив из 8 эллементов ");
+Console.WriteLine($"[{string.Join(",", array)}]");
