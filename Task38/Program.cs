68ec3a6553536c044c8e8a6c8097468a3e4f783b
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-17 15:13:23 +0300

    Add Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,32 @@
+﻿void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(1, 50);
+}
+
+
+double Random(double[] array)
+{
+   double diff =0;
+    double max = array[0];
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > max)
+            max = array[i];
+        if (array[i] < min)
+            min = array[i];
+
+    }
+    diff =max -min;
+    return diff;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество числе в массиве ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[a];
+
+FillArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine($"Разница между максимальным и минимальным числом массива: {Random(array)}");
