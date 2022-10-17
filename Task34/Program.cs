c84621c97fab2cdded70cb20fc9033f0f0ab0996
Svetlana <sveta.yakovenko97@mail.ru>
2022-10-17 14:40:42 +0300

    Add Task34

---
+++ Task34/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+
+int SummNembers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count = count + 1;
+
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество числе в массиве ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+FillArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine($"Количество четных чисел: {SummNembers(array)}");
