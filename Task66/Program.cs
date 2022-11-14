5d439b611b6405292eee6d2560ef9f3809ab1f20
Svetlana <sveta.yakovenko97@mail.ru>
2022-11-14 17:56:29 +0300

    add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int RecursiveSum(int a, int b)
+{
+    if (a == b)
+        return a;
+
+    return a + RecursiveSum(a + 1, b);
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(RecursiveSum(a, b));
+
+
+
+
+ 
